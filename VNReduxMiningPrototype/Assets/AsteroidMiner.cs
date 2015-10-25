4cbf1249ef4ba83e0d3f8d51c87d3f9e80cab208
Marjie <marjie.volk@gmail.com>
2015-10-25 16:03:15 -0700

    Ship can now mine things with 'M' when close to the asteroid.

---
+++ VNReduxMiningPrototype/Assets/AsteroidMiner.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidMiner : MonoBehaviour {
+
+    public float MiningDistance;
+    public float MiningRate;
+
+    private ResourceTank _tank;
+    private Mineable _target;
+    private float _sqMiningDistance;
+
+	// Use this for initialization
+	void Start () {
+        ResourceTank[] tanks = GetComponents<ResourceTank>();
+        foreach(ResourceTank tank in tanks)
+        {
+            if (tank.Type == Resource.ORE) _tank = tank;
+        }
+        _sqMiningDistance = MiningDistance * MiningDistance;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            tryAcquireTarget();
+        }
+
+        checkRange();
+
+        if(_target != null)
+        {
+            if (_tank != null && _tank.Stored < _tank.Capacity)
+            {
+                _tank.BestEffortStore(MiningRate * Time.deltaTime);
+            }
+        }
+    }
+
+    private void tryAcquireTarget()
+    {
+        // find closest asteroid
+        Mineable[] asteroids = FindObjectsOfType<Mineable>();
+        float minDistance = float.MaxValue;
+        _target = null;
+        foreach (Mineable target in asteroids)
+        {
+            float distance = (target.transform.position - transform.position).sqrMagnitude;
+            if (distance < minDistance)
+            {
+                _target = target;
+                minDistance = distance;
+            }
+        }
+
+        // if it's too far away, don't start mining
+        if (minDistance > _sqMiningDistance) _target = null;
+    }
+
+    private void checkRange()
+    {
+        if (_target == null) return;
+        float sqDistance = (_target.transform.position - transform.position).sqrMagnitude;
+        if (sqDistance > _sqMiningDistance)
+        {
+            tryAcquireTarget();
+        }
+    }
+}
---
+++ VNReduxMiningPrototype/Assets/ResourceTank.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ResourceTank : MonoBehaviour {
+
+    //Editor-configurables
+    public Resource Type;
+    public float Capacity;
+    public Text Display;
+
+    public float Stored { get; private set; }
+
+    public float BestEffortStore(float amount)
+    {
+        amount = Mathf.Max(amount, 0.0f);
+        float remainingCapacity = Capacity - Stored;
+        float taken = Mathf.Min(amount, remainingCapacity);
+        Stored += taken;
+        return amount - taken;
+    }
+
+    public float BestEffortExtract(float amount)
+    {
+        amount = Mathf.Max(amount, 0.0f);
+        float taken = Mathf.Min(amount, Stored);
+        Stored -= taken;
+        return taken;
+    }
+
+	// Use this for initialization
+	void Start () {
+        Stored = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Display != null)
+        {
+            Display.text = Type.ToString() + ": " + Stored;
+        }
+	}
+}
