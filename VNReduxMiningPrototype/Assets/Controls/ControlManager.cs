aac80d13e2fce583385b2bc41857ab8f667cfcd3
Marjie <marjie.volk@gmail.com>
2015-10-25 19:08:26 -0700

    The beginnings and middles of a more flexible control scheme

---
+++ VNReduxMiningPrototype/Assets/Controls/ControlManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ControlManager : MonoBehaviour {
+
+    private HashSet<ControlScheme> enabledControls;
+    
+	void Start () {
+        enabledControls = new HashSet<ControlScheme>();
+	}
+	
+	void Update () {
+	    foreach (ControlScheme controlScheme in enabledControls) {
+            foreach (KeyCode keyCode in controlScheme.getKeyHandlers().Keys) {
+                if (Input.GetKey(keyCode)) {
+                    controlScheme.getKeyHandlers()[keyCode]();
+                }
+            }
+
+            foreach (KeyCode keyCode in controlScheme.getKeyDownHandlers().Keys) {
+                if (Input.GetKeyDown(keyCode)) {
+                    controlScheme.getKeyDownHandlers()[keyCode]();
+                }
+            }
+
+            foreach (KeyCode keyCode in controlScheme.getKeyUpHandlers().Keys) {
+                if (Input.GetKeyUp(keyCode)) {
+                    controlScheme.getKeyUpHandlers()[keyCode]();
+                }
+            }
+        }
+	}
+
+    public void enableControlScheme(ControlScheme controlScheme) {
+        enabledControls.Add(controlScheme);
+    }
+
+    public void disableControlScheme(ControlScheme controlScheme) {
+        enabledControls.Remove(controlScheme);
+    }
+}
---
+++ VNReduxMiningPrototype/Assets/Controls/ShipControlScheme.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class ShipControlScheme : ControlScheme {
+
+    public float TorquePower;
+    public float AngularSpeedFloor;
+
+    private Dictionary<KeyCode, Command> keyDownHandlers;
+    private Dictionary<KeyCode, Command> keyUpHandlers;
+    private Dictionary<KeyCode, Command> keyHandlers;
+
+    void Start() {
+        keyDownHandlers = new Dictionary<KeyCode, Command>();
+        keyUpHandlers = new Dictionary<KeyCode, Command>();
+        keyHandlers = new Dictionary<KeyCode, Command>();
+
+        Rigidbody _target = GetComponent<Rigidbody>();
+
+        // Linear controls
+        keyHandlers.Add(KeyCode.I, () => { _target.AddRelativeForce(Vector3.forward); });
+        keyHandlers.Add(KeyCode.K, () => { _target.AddRelativeForce(Vector3.back); });
+        keyHandlers.Add(KeyCode.J, () => { _target.AddRelativeForce(Vector3.left); });
+        keyHandlers.Add(KeyCode.L, () => { _target.AddRelativeForce(Vector3.right); });
+        keyHandlers.Add(KeyCode.U, () => { _target.AddRelativeForce(Vector3.up); });
+        keyHandlers.Add(KeyCode.O, () => { _target.AddRelativeForce(Vector3.down); });
+
+        // Torque controls
+        keyHandlers.Add(KeyCode.W, () => { _target.AddRelativeTorque(Vector3.right * TorquePower); });
+        keyHandlers.Add(KeyCode.S, () => { _target.AddRelativeTorque(Vector3.left * TorquePower); });
+        keyHandlers.Add(KeyCode.A, () => { _target.AddRelativeTorque(Vector3.down * TorquePower); });
+        keyHandlers.Add(KeyCode.D, () => { _target.AddRelativeTorque(Vector3.up * TorquePower); });
+        keyHandlers.Add(KeyCode.Q, () => { _target.AddRelativeTorque(Vector3.forward * TorquePower); });
+        keyHandlers.Add(KeyCode.E, () => { _target.AddRelativeTorque(Vector3.back * TorquePower); });
+
+        // Angular dampening
+        keyHandlers.Add(KeyCode.F, () => {
+            if (_target.angularVelocity.magnitude < AngularSpeedFloor) {
+                _target.angularVelocity = Vector3.zero;
+            } else {
+                _target.AddTorque(-1 * TorquePower * _target.angularVelocity.normalized);
+            }
+        });
+
+    }
+
+    public override Dictionary<KeyCode, Command> getKeyDownHandlers() {
+        return keyDownHandlers;
+    }
+
+    public override Dictionary<KeyCode, Command> getKeyUpHandlers() {
+        return keyUpHandlers;
+    }
+
+    public override Dictionary<KeyCode, Command> getKeyHandlers() {
+        return keyHandlers;
+    }
+}
