e747841a738a95196e2a784d7266c019d7cc6f8b
knexer <knexer@gmail.com>
2015-11-07 11:58:21 -0800

    Construction queue display skeleton

---
+++ VNReduxMiningPrototype/Assets/ConstructionQueueDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Displays the ships currently under construction and their progress / ETA.
+/// </summary>
+public class ConstructionQueueDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Ship.ShipSelected += generateDisplay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void generateDisplay(Ship ship)
+    {
+        Shipyard constructionManager = ship.GetComponent<Shipyard>();
+        if (null == constructionManager)
+        {
+            hide();
+        }
+    }
+
+    private void hide()
+    {
+    }
+
+    private void show()
+    {
+    }
+}
